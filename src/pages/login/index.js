c3dd76f440382df6d8a2f5ad22e7458de543989e
Jeff Reeh <65681066+jareeh@users.noreply.github.com>
2021-02-17 13:46:06 -0600

    add login page with form control

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Layout from '../../Components/Layout';
 
- export default function Login() {
+export default function Login() {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const updateEmail = (e) => {
+        setEmail(e.target.value);
+    };
+    const updatePassword = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
-        <div>
-            This is the login page.
-        </div>
-    )
+        <Layout>
+            <div className="container mx-auto">
+                <div className="">
+                    <h2 className="">Login:</h2>
+                    <form className="" onSubmit={handleSubmit}>
+                        <label>
+                            Email Address:
+                            <input type="text" className="" onChange={updateEmail} />
+                        </label>
+                        <label>
+                            Password:
+                            <input type="password" className="" onChange={updatePassword} />
+                        </label>
+                        <br />
+                        <button type="submit" className="">
+                            Login
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </Layout>
+    );
 }
-
-
