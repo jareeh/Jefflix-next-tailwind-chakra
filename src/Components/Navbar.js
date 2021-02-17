956088440c9eefedf45676e3a09ebe2fae9d17f0
Jeff Reeh <65681066+jareeh@users.noreply.github.com>
2021-02-17 13:46:39 -0600

    prettier

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,33 +1,31 @@
 import React, { useState, useEffect, useContext } from 'react';
 import Link from 'next/link';
 
-export default function NavBar(){
+export default function NavBar() {
     return (
-        <>
-            <nav className="flex items-center flex-wrap bg-gray-900 p-3 ">
-                <Link href="/">
-                    <a className="inline-flex items-center p-2 mr-4 ">
-                        <span className="text-xl text-white font-bold uppercase tracking-wide">
-                            Jefflix
-                        </span>
-                        <span></span>
-                    </a>
-                </Link>
-                <Link href="/login">
-                    <a className="inline-flex items-center p-2 mr-4">
-                        <span className="text-xl text-white font-bold uppercase tracking-wide">
-                            Login
-                        </span>
-                    </a>
-                </Link>
-                <Link href="/register">
-                    <a className="inline-flex items-center p-2 mr-4">
-                        <span className="text-xl text-white font-bold uppercase tracking-wide">
-                            Register
-                        </span>
-                    </a>
-                </Link>
-            </nav>
-        </>
+        <nav className="flex flex-wrap content-evenly bg-gray-900 p-3 ">
+            <Link href="/home">
+                <a className="inline-flex items-center p-2 mr-4 ">
+                    <span className="text-xl text-white font-bold uppercase tracking-wide">
+                        Jefflix
+                    </span>
+                    <span></span>
+                </a>
+            </Link>
+            <Link href="/login">
+                <a className="inline-flex items-center p-2 mr-4">
+                    <span className="text-xl text-white font-bold uppercase tracking-wide">
+                        Login
+                    </span>
+                </a>
+            </Link>
+            <Link href="/register">
+                <a className="inline-flex items-center p-2 mr-4">
+                    <span className="text-xl text-white font-bold uppercase tracking-wide">
+                        Register
+                    </span>
+                </a>
+            </Link>
+        </nav>
     );
-};
+}
